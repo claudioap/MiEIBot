c46fd6889f95c8b9538224152e33b3e947c8672d
Cláudio Pereira <git@claudiop.com>
2017-12-24 03:53:32 +0000

    Initial course crawling (id, names, creation and end years)

--- clip/utils/populate.py
+++ clip/utils/populate.py
@@ -22,6 +22,14 @@ url_institution_years = 'https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade
 url_departments = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo?ano_lectivo={}&institui%E7%E3o={}"
 url_classes = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo/sector?institui%E7%E3o={}&ano_lectivo={}&sector={}"
 url_classes_period = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo/sector/ano_lectivo?tipo_de_per%EDodo_lectivo={}&sector={}&ano_lectivo={}&per%EDodo_lectivo={}&institui%E7%E3o={}"
+url_courses = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/curso?institui%E7%E3o={}"
+url_curricular_plans = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/curso?institui%E7%E3o={}&curso={}"
+url_admissions = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo/candidaturas?ano_lectivo={}&institui%E7%E3o={}"
+url_admitted = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo/candidaturas/colocados?ano_lectivo={}&institui%E7%E3o={}&fase={}&curso={}"
+url_class_enrolled = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo/sector/ano_lectivo/unidade_curricular/actividade/inscri%E7%F5es/pautas?tipo_de_per%EDodo_lectivo={}&sector={}&ano_lectivo={}&per%EDodo_lectivo={}&institui%E7%E3o={}&unidade_curricular={}&modo=pauta&aux=ficheiro"
+url_class_exam_enrolled_file = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo/sector/ano_lectivo/unidade_curricular/actividade/testes_de_avalia%E7%E3o/inscritos?institui%E7%E3o={}&%EDndice={}&sector={}&ano_lectivo={}&tipo_de_per%EDodo_lectivo={}&tipo={}&per%EDodo_lectivo={}&unidade_curricular={}&%E9poca={}&aux=ficheiro"
+url_exam_grades = "https://clip.unl.pt/utente/institui%E7%E3o_sede/unidade_organica/ensino/ano_lectivo/sector/ano_lectivo/unidade_curricular/actividade/testes_de_avalia%E7%E3o/inscritos?tipo_de_per%EDodo_lectivo={}&sector={}&ano_lectivo={}&per%EDodo_lectivo={}&institui%E7%E3o={}&unidade_curricular={}&%E9poca={}&tipo={}&%EDndice={}"
+
 session = Session()
 
 
@@ -286,6 +294,84 @@ def populate_classes(session, database):
         print("Changes saved")
 
 
+def populate_courses_prelimiar(session, database):
+    db_cursor = database.cursor()
+    for institution in institutions:
+        courses = {}
+        soup = request_to_soup(session.get(url_courses.format(institution)))
+        course_exp = re.compile("\\bcurso=\\d+\\b")
+        course_links = soup.find_all(href=course_exp)
+        for course_link in course_links:
+            course_iid = course_exp.findall(course_link.attrs['href'])[0].split('=')[-1]
+            courses[course_iid] = {
+                'name': course_link.contents[0].text.strip(),
+                'initial_year': None,
+                'final_year': None
+            }
+            soup = request_to_soup(session.get(url_curricular_plans.format(institution, course_iid)))
+            year_links = soup.find_all(href=re.compile("\\bano_lectivo=\\d+\\b"))
+            for year_link in year_links:
+                year = int(year_link.attrs['href'].replace('\n', ' ').split('=')[-1])
+                if courses[course_iid]['initial_year'] is None:
+                    courses[course_iid]['initial_year'] = year
+                    courses[course_iid]['final_year'] = year
+                elif courses[course_iid]['initial_year'] > year:
+                    courses[course_iid]['initial_year'] = year
+                elif courses[course_iid]['final_year'] < year:
+                    courses[course_iid]['final_year'] = year
+
+        # add to db
+        for course_iid, course_info in courses.items():
+            db_cursor.execute('SELECT name, initial_year, final_year '
+                              'FROM Courses '
+                              'WHERE institution=? AND internal_id=?',
+                              (institutions[institution]['id'], course_iid))
+            exists = False
+            different = False
+            for row in db_cursor.fetchall():
+                course_name = row[0]
+                course_initial_year = row[1]
+                course_final_year = row[2]
+                if course_name != course_info['name']:
+                    raise Exception("Different courses had an id collision")
+
+                # creation date different or  last year different
+                elif course_initial_year != course_info['initial_year'] \
+                        or course_final_year != course_info['final_year']:
+                    different = True
+                    exists = True
+
+            if not exists:  # unknown department, add it to the DB
+                print("Adding  {}({}) {} - {} to the database".format(
+                    course_info['name'],
+                    course_iid,
+                    course_info['initial_year'],
+                    course_info['final_year']))
+                db_cursor.execute(
+                    'INSERT INTO Courses(internal_id, name, initial_year, final_year, institution) '
+                    'VALUES (?, ?, ?, ?, ?)',
+                    (course_iid,
+                     course_info['name'],
+                     course_info['initial_year'],
+                     course_info['final_year'],
+                     institutions[institution]['id']))
+
+            if different:  # department changed
+                print("Updating course {} (now goes from {} to {})".format(
+                    course_info['name'], course_info['initial_year'], course_info['final_year']))
+                db_cursor.execute(
+                    'UPDATE Courses '
+                    'SET initial_year = ?, final_year=? '
+                    'WHERE internal_id=? AND institution=?',
+                    (course_info['initial_year'],
+                     course_info['final_year'],
+                     course_iid,
+                     institutions[institution]['id']))
+
+        database.commit()
+        print("Changes saved")
+
+
 def request_to_soup(request):
     soup = BeautifulSoup(request.text, 'html.parser')
     # Take useless stuff out of the way for better debugging.
@@ -304,5 +390,6 @@ def request_to_soup(request):
     return soup
 
 
-# populate_departments(session, database)
+populate_departments(session, database)
 populate_classes(session, database)
+populate_courses_prelimiar(session, database)
